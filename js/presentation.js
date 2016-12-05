edb6a7d5405e649bb2ff958f2a7600c348076cc6
Hamza Ali <hamzali.tas.95@gmail.com>
2016-12-05 12:44:06 +0300

    js update

--- js/presentation.js
+++ js/presentation.js
@@ -1,8 +1,8 @@
 let pages = $('.page').addClass('hidden');
 
 const SPACE = 32;
-const KEY_A = 97;
-const KEY_D = 100;
+const KEY_A = 65;
+const KEY_D = 68;
 
 function show(elem) {
     elem.className = elem.className.split(' ').slice(0, 2).join(' ');
@@ -18,15 +18,17 @@ let currentPage = 0;
 
 $(window).keypress(function (e) {
     let lastPage = currentPage;
-    switch(e.keyCode) {
-        case SPACE:
+    console.dir(e);
+    console.log(e.key);
+    switch(e.key) {
+        case 'd':
             if(currentPage >= pages.length - 1) currentPage = 0;
             else currentPage++;
 
             hide(pages[lastPage]);
             show(pages[currentPage]);
             break;
-        case KEY_A:
+        case 'a':
             if(currentPage <= 0) currentPage = pages.length - 1;
             else currentPage--;
 
